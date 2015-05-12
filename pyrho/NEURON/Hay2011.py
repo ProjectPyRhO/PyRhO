07c443bd4fb46b63848bfdd84c9f0460256c55a0
Ben Evans <ben.d.evans@gmail.com>
2015-05-12 11:32:54 +0100

    Tidying up moved/renamed files

--- pyrho/NEURON/Hay2011.py
+++
@@ -1,9 +0,0 @@
-# Hay2011.py
-
-#['import3d.hoc', 'models/L5PCbiophys3.hoc', 'models/L5PCtemplate.hoc']
-self.h.load_file('import3d.hoc')
-self.h.load_file('models/L5PCbiophys3.hoc')
-self.h.load_file('models/L5PCtemplate.hoc')
-self.h('objref L5PC') #h('objref '+cell)
-self.h.L5PC = self.h.L5PCtemplate("morphologies/cell1.asc")
-return self.h.L5PC
