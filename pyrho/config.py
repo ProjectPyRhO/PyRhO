3653fdf4c68665fe7085d9a971a5d743aef0f0a5
Ben Evans <ben.d.evans@gmail.com>
2015-08-19 11:28:58 +0100

    Changed figure formatting into a function setFigOutput() and added some variables for extracting NMDOL files.

--- pyrho/config.py
+++ pyrho/config.py
@@ -24,16 +24,22 @@ if not os.path.isdir(fDir):
     createDir(fDir)
 
 def createDir(path):
+    if os.path.isdir(path):
+        return
     try:
         os.makedirs(path)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise    
     
+#pyrhoPath = os.path.abspath(pyrho.__file__) # http://stackoverflow.com/questions/247770/retrieving-python-module-path
+NMODLfiles = ['RhO3.mod', 'RhO4.mod', 'RhO6.mod']
+#pyrhoNEURONpath = os.path.join(pyrhoPath, 'NEURON')
+#NMODLfilesIncPath = [os.path.join(pyrhoNEURONpath, f) for f in NMODLfiles]
     
 ##### Plot settings #####
 
-display = 'screen' #'screen' #'paper'
+display = 'screen' #'paper'
 
 colours = ['b','g','r','c','m','y','k']
 styles = ['-', '--', '-.', ':']
@@ -45,61 +51,70 @@ if latexInstalled:
     mp.rcParams['text.usetex'] = True
 #mp.rcParams.update(rcdef) # Set back to defaults
 
-golden_ratio  = (1.0 + np.sqrt(5)) / 2.0
+def setFigOutput(display='screen'):
+    golden_ratio  = (1.0 + np.sqrt(5)) / 2.0
 
-# http://matplotlib.org/users/customizing.html
-if display == 'screen':
-    dpi = 300
-    #figFormat = 'retina'
-    saveFigFormat = 'png'    
-    figWidth = 12
-    figHeight = figWidth / golden_ratio
-    tickSize = 14
-    labelSize = 16
-    legendSize = 12
-    titleSize = 'x-large'
-    eqSize = 18
-    linewidth = 1.5
-    addTitles = True
-    addStimulus = True
+    # http://matplotlib.org/users/customizing.html
+    if display == 'screen':
+        dpi = 300 #mp.rcParams['savefig.dpi'] #300
+        #figFormat = 'retina'
+        saveFigFormat = 'png'    
+        figWidth = 12
+        figHeight = figWidth / golden_ratio
+        tickSize = 14
+        labelSize = 16
+        legendSize = 12
+        titleSize = 'small' #'x-large'
+        eqSize = 18
+        linewidth = 1.5
+        addTitles = True
+        addStimulus = True
 
-elif display == 'paper':
-    #http://www.nature.com/nature/authors/gta/3c_Final_artwork.pdf
-    #http://www.plosone.org/static/figureSpecifications
-    #http://www.ploscompbiol.org/static/figureSpecifications
-    dpi = 600 # Set DPI 300 - 600
-    saveFigFormat = 'png'#'eps'           # Supported formats: eps, pdf, pgf, png, ps, raw, rgba, svg, svgz.
-    #figFormat = 'eps'
-    figWidth = 5#7
-    figHeight = figWidth / golden_ratio
-    tickSize = 6
-    labelSize = 7
-    legendSize = 7
-    titleSize = 'medium'
-    eqSize = 7
-    linewidth = 1
-    addTitles = False
-    addStimulus = True
+    elif display == 'paper':
+        #http://www.nature.com/nature/authors/gta/3c_Final_artwork.pdf
+        #http://www.plosone.org/static/figureSpecifications
+        #http://www.ploscompbiol.org/static/figureSpecifications
+        #http://www.frontiersin.org/about/AuthorGuidelines#_FigureandTableGuidelines
+        dpi = 1200 # Set DPI 300 - 600
+        saveFigFormat = 'png'#'eps'             # Supported formats: eps, pdf, pgf, png, ps, raw, rgba, svg, svgz.
+        #figFormat = 'eps'
+        figWidth = 18/2.54 #5#7
+        figHeight = figWidth / golden_ratio
+        tickSize = 8
+        labelSize = 10
+        legendSize = 10
+        titleSize = 'medium'
+        eqSize = 12
+        linewidth = 2
+        addTitles = False
+        addStimulus = True
 
-else:
-    warnings.warn('Warning: Unknown display type selected - using matplotlib defaults!')
+        mp.rcParams['savefig.dpi'] = dpi        # http://nbviewer.ipython.org/gist/minrk/3301035
+                                                # http://wiki.scipy.org/Cookbook/Matplotlib/AdjustingImageSize
+        
+    else:
+        warnings.warn('Warning: Unknown display type selected - using matplotlib defaults!')
+    
+    # http://matplotlib.org/users/customizing.html
+    mp.rcParams['figure.figsize'] = (figWidth, figHeight) #pylab.rcParams
+    mp.rcParams['figure.dpi'] = dpi
+    #mp.rcParams['savefig.dpi'] = dpi           # http://nbviewer.ipython.org/gist/minrk/3301035
+    mp.rcParams['axes.labelsize'] = labelSize
+    mp.rcParams['ytick.labelsize'] = tickSize
+    mp.rcParams['xtick.labelsize'] = tickSize
+    mp.rcParams['legend.fontsize'] = legendSize
+    mp.rcParams['axes.titlesize'] = titleSize
+    mp.rcParams['lines.linewidth'] = linewidth
+    mp.rcParams['font.size'] = eqSize
+    
+    
+    return saveFigFormat, addTitles, addStimulus, eqSize
 
-mp.rcParams['figure.figsize'] = (figWidth, figHeight) #pylab.rcParams
-mp.rcParams['figure.dpi'] = dpi
-#mp.rcParams['savefig.dpi'] = dpi           # http://nbviewer.ipython.org/gist/minrk/3301035
-mp.rcParams['axes.labelsize'] = labelSize
-mp.rcParams['ytick.labelsize'] = tickSize
-mp.rcParams['xtick.labelsize'] = tickSize
-mp.rcParams['legend.fontsize'] = legendSize
-mp.rcParams['axes.titlesize'] = titleSize
-mp.rcParams['lines.linewidth'] = linewidth
+# plotPeakRecovery = False
+# plotStateVars = False
+# plotKinetics = False
 
-
-plotPeakRecovery = False
-plotStateVars = False
-plotKinetics = False
-
-
+saveFigFormat, addTitles, addStimulus, eqSize = setFigOutput(display)
 
 try:
     __IPYTHON__
