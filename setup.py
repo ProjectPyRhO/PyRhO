e0019d5abbe3f0292a5de961b584384b9a70dcf9
Ben Evans <ben.d.evans@gmail.com>
2016-03-03 18:08:40 +0000

    Paste in long_description

--- setup.py
+++ setup.py
@@ -20,9 +20,34 @@ prwd = os.path.join(home, 'pyrho') # pyrho working directory
 
 # Get the long description from the relevant file
 #with open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-with open('DESCRIPTION.rst', encoding='utf-8') as f:
-    long_description = f.read()
+#with open('DESCRIPTION.rst', encoding='utf-8') as f:
+#    long_description = f.read()
 
+long_description = """
+PyRhO - A Virtual Optogenetics Laboratory
+=========================================
+
+A Python module to fit and characterise rhodopsin photocurrents
+
+Optogenetics has become a key tool for understanding the function of neural circuits and controlling their behaviour. An array of directly light driven opsins have been genetically isolated from several families of organisms, with a wide range of temporal and spectral properties. In order to characterize, understand and apply these rhodopsins, we present an integrated suite of open-source, multi-scale computational tools called PyRhO. 
+
+The purpose of developing PyRhO is threefold: 
+
+(i) to characterize new (and existing) rhodopsins by automatically fitting a minimal set of experimental data to three, four or six-state kinetic models, 
+(ii) to simulate these models at the channel, neuron & network levels and 
+(iii) provide functional insights through model selection and virtual experiments *in silico*. 
+
+The module is written in Python with an additional IPython/Jupyter notebook based GUI, allowing models to be fit, simulations to be run and results to be shared through simply interacting with a webpage. The seamless integration of model fitting algorithms with simulation environments for these virtual opsins will enable neuroscientists to gain a comprehensive understanding of their behaviour and rapidly identify the most suitable variant for application in a particular biological system. This process may thereby guide not only experimental design and opsin choice but also alterations of the rhodopsin genetic code in a neuro-engineering feed-back loop. In this way, we expect PyRhO will help to significantly improve optogenetics as a tool for transforming biological sciences. 
+
+If you use PyRhO please cite our paper: 
+
+Evans, B. D., Jarvis, S., Schultz, S. R. & Nikolic K. (2016) "PyRhO: A Multiscale Optogenetics Simulation Platform", *Front. Neuroinform., 10* (8). `doi:10.3389/fninf.2016.00008 <https://dx.doi.org/10.3389/fninf.2016.00008>`_
+
+The PyRhO project website with additional documentation may be found here: `www.imperial.ac.uk/bio-modelling/pyrho <http://www.imperial.ac.uk/a-z-research/bio-modelling/pyrho>`_
+
+
+"""
+    
 
 setup(
     name='PyRhO',
@@ -30,7 +55,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.2',
+    version='0.9.3',
 
     description='Fit and characterise rhodopsin photocurrents',
     long_description=long_description,
@@ -38,7 +63,7 @@ setup(
     # The project's main homepage.
     url='https://github.com/ProjectPyRhO/PyRhO/', 
     #download_url='https://github.com/ProjectPyRhO/PyRhO/archive/master.zip', 
-    #download_url='https://github.com/ProjectPyRhO/PyRhO/tarball/0.9.1',
+    #download_url='https://github.com/ProjectPyRhO/PyRhO/tarball/0.9.3',
     
     # Author details
     author='Benjamin D. Evans',
