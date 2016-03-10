798d6df6b083e818abadf97ac6897b939f71fd81
Ben Evans <ben.d.evans@gmail.com>
2016-03-10 20:15:38 +0000

    More PEP8 work

--- pyrho/__init__.py
+++ pyrho/__init__.py
@@ -1,59 +1,63 @@
 """A Python module for fitting, characterising and simulating rhodopsin photocurrents"""
-#__doc__ = 
+#__doc__ =
 # Main module file for PyRhO
 
 #if sys.version_info < (3,0) #IPython.__version__ < "3":
 from __future__ import division # a/b -> float
-from __future__ import absolute_import, print_function, unicode_literals 
+from __future__ import absolute_import, print_function, unicode_literals
 
 import platform
-IPY = False
-try:
-    import IPython
-    IPY = True
-except ImportError:
-    IPY = False
-
+import os
 from pkg_resources import get_distribution, DistributionNotFound
 
 # Necessary?
-import matplotlib as mpl   
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 import lmfit
 
 
+#IPY = False
+#try:
+#    import IPython
+#    IPY = True
+#except ImportError:
+#    IPY = False
+
 # Place all submodule functions and variables into namespace
 from pyrho.config import *
 from pyrho.parameters import *
 from pyrho.utilities import *
-from pyrho.expdata import * #import loadData
-from pyrho.models import * #import models
+from pyrho.expdata import *
+from pyrho.models import *
 from pyrho.simulators import *
-from pyrho.protocols import * #import modProtocols
-from pyrho.fitting import * #import fitStates
-from pyrho.jupytergui import * # IPythonGUI
+from pyrho.protocols import *
+from pyrho.fitting import *
+from pyrho.jupytergui import *
 
 #TODO
-#__all__ = ['config', 'utilities', 'parameters', 'expdata', 'models', 'protocols', 'simulators', 'fitting', 'IPythonGUI']
+#__all__ = ['config', 'utilities', 'parameters', 'expdata',
+#            'models', 'protocols', 'simulators', 'fitting', 'jupytergui']
 
 __project__ = 'pyrho'
 # http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
 #__version__ = get_distribution(__project__).version #'0.8.0'
 try:
-    import os
     _dist = get_distribution(__project__)
-    distLoc = os.path.normcase(_dist.location) # Normalise case for Windows systems
+    distLoc = os.path.normcase(_dist.location) # Normalise case for Windows
     here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(distLoc, __project__)): # not installed, but there is another version that *is*
+    if not here.startswith(os.path.join(distLoc, __project__)):
+        # not installed, but there is another version that *is*
         raise DistributionNotFound
 except DistributionNotFound:
     __version__ = __project__ + '-' + '(local)'
 else:
     __version__ = _dist.version
 
-if __name__ == '__main__': 
+IPY = check_package('IPython')
+
+if __name__ == '__main__':
     try:
         __IPYTHON__
     except NameError:
@@ -63,17 +67,19 @@ if __name__ == '__main__':
         loadGUI()
 
 # TODO Move everything except imports elsewhere
-        
+
+
 def runAll(listOfModels=[6]):
-    """Run all protocols with the Python simulator on a list of models with default parameters!
-    listOfModels    := individual or list of integers or strings 
-                        e.g. [3,4,6], 3, '4', ['4','6'], modelList
     """
-    
+    Run all protocols (with Python) on a list of models with default parameters!
+    listOfModels    := individual or list of integers or strings
+                        e.g. [3, 4, 6], 3, '4', ['4', '6'], modelList
+    """
+
     if not isinstance(listOfModels, (list, tuple)):
         listOfModels = [listOfModels] # ints or strs
     listOfModels = [str(m) for m in listOfModels]
-    
+
     for model in listOfModels:
         ### Select generative model
         RhO = models[model]()
@@ -81,23 +87,23 @@ def runAll(listOfModels=[6]):
             ### Select simulation protocol
             Prot = protocols[prot]()
             for sim in ['Python']:#simulators:
-                Sim = simulators[sim](Prot, RhO)            
+                Sim = simulators[sim](Prot, RhO)
                 print("\nRunning Protocol '{}' on the {}-state model...".format(prot, model))
-                print('--------------------------------------------------------------------------------\n')
-                Sim.run()                
+                print(DASH_LINE, '\n')
+                Sim.run()
                 Sim.plot()
                 print("\nFinished!")
-                print('================================================================================\n\n')
+                print(DOUB_DASH_LINE, '\n\n')
 
-                
+
 def printVersions():
     """Display version information for PyRhO and its dependencies"""
 
     #import platform
     print("Python version: ", platform.python_version())
-    if IPY:    
+    if IPY:
         try:
-            #import IPython
+            import IPython
             #__IPYTHON__
             print("IPython version: ", IPython.__version__)
         except ImportError: # IPython not found
@@ -114,6 +120,6 @@ def printVersions():
     print("Matplotlib version: ", mpl.__version__)
     #import lmfit
     print("Lmfit version: ", lmfit.__version__)
-    
+
     print("PyRhO version: ", __version__)
 
