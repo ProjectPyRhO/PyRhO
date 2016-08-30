6be60e2073d3336a2d691b0c3f9b5d2c983381a7
Ben Evans <ben.d.evans@gmail.com>
2016-08-30 01:45:27 +0100

    Add boilerplate for importing gui libraries

--- pyrho/gui/__init__.py
+++ pyrho/gui/__init__.py
@@ -1,3 +1,11 @@
 from pkg_resources import resource_filename
 
 #resource_filename(package_or_requirement, resource_name)
+
+#TODO: Boilerplate for widgets and notebook environment
+try:
+    import ipywidgets as widgets
+    from IPython.display import display
+    from IPython.display import clear_output
+except ImportError:
+    widgets = None
