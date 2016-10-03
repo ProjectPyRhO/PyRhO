089c9ac9fe948a4358c4dc824e70a944c9d11166
Ben Evans <ben.d.evans@gmail.com>
2016-10-03 14:50:33 +0100

    Remove support for pyVer<3.3 for compatibility with py.test

--- setup.py
+++ setup.py
@@ -95,11 +95,13 @@ setup(
         # Supported Python versions
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        # 2.6 barriers: OrderedDict, dictionary comprehension, importlib
         #'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
+        # Technically 3.1 and 3.2 should work but are unsupported by Travis
+        #'Programming Language :: Python :: 3.1',
+        #'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
